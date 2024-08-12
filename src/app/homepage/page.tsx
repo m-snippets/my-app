a5e784552f1671c451eb10617b17caddd0aaecfc
m-snippets <muhammad.taha2128@gmail.com>
2024-08-12 22:35:09 +0500

    chore: Add address page and link to homepage

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+function homepage() {
+  return (
+    <div>homepage
+          <Link href="/namepage">namepage</Link>
+    </div>
+  
+  )
+}
+
+export default homepage
---
+++ src/app/namepage/address/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function address() {
+  return (
+    <div>i live in pakistan</div>
+  )
+}
+
+export default address
